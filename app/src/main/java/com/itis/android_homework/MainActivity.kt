618dd4813318f14b93d6590a00e22c349ad9e130
AnastasiaChukhina <nas2002@list.ru>
2021-10-05 15:23:33 +0300

    Screen layoit is fully done

--- app/src/main/java/com/itis/android_homework/MainActivity.kt
+++ app/src/main/java/com/itis/android_homework/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.itis.android_homework
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.close
+import android.view.View
+import android.view.Window
+import android.view.inputmethod.InputMethodManager
+import com.itis.android_homework.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        supportActionBar?.hide()
+
+        binding.btnEditProfileName.setOnClickListener {
+            changeNickname(it)
+        }
+    }
+
+    private fun changeNickname(view: View) {
+        shoeEditText()
+        binding.btnDone.setOnClickListener {
+            showUsername()
+            closeTab(it)
+        }
+    }
+
+    private fun closeTab(view: View?) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
+
+    private fun shoeEditText(){
+        binding.apply {
+            tvNick.visibility = View.GONE
+            etUsername.visibility = View.VISIBLE
+            btnDone.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showUsername(){
+        binding.apply {
+            tvNick.text = etUsername.text
+            tvNick.visibility = View.VISIBLE
+            etUsername.visibility = View.GONE
+            etUsername.text.clear()
+            btnDone.visibility = View.GONE
+        }
     }
 }
